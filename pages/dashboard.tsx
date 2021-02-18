3e57208b998531a85b69bae361a7ca0ba012a676
jakequinter <500132870@fvtc.edu>
2021-02-18 16:54:04 -0500

    fixed invalid class names

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -5,19 +5,19 @@ import DashboardShell from '../components/DashboardShell';
 const dashboard = () => {
   return (
     <DashboardShell>
-      <div class="h-screen flex overflow-hidden">
-        <div class="md:hidden">
-          <div class="fixed inset-0 flex z-40">
-            <div class="fixed inset-0">
-              <div class="absolute inset-0 bg-gray-600 opacity-75"></div>
+      <div className="h-screen flex overflow-hidden">
+        <div className="md:hidden">
+          <div className="fixed inset-0 flex z-40">
+            <div className="fixed inset-0">
+              <div className="absolute inset-0 bg-gray-600 opacity-75"></div>
             </div>
 
-            <div class="relative flex-1 flex flex-col max-w-xs w-full bg-white">
-              <div class="absolute top-0 right-0 -mr-12 pt-2">
-                <button class="ml-1 flex items-center justify-center h-10 w-10 rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                  <span class="sr-only">Close sidebar</span>
+            <div className="relative flex-1 flex flex-col max-w-xs w-full bg-white">
+              <div className="absolute top-0 right-0 -mr-12 pt-2">
+                <button className="ml-1 flex items-center justify-center h-10 w-10 rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <span className="sr-only">Close sidebar</span>
                   <svg
-                    class="h-6 w-6 text-white"
+                    className="h-6 w-6 text-white"
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
                     viewBox="0 0 24 24"
@@ -33,14 +33,14 @@ const dashboard = () => {
                   </svg>
                 </button>
               </div>
-              <div class="flex-1 h-0 pt-5 pb-4 overflow-y-auto">
-                <nav class="mt-5 px-2 space-y-1">
+              <div className="flex-1 h-0 pt-5 pb-4 overflow-y-auto">
+                <nav className="mt-5 px-2 space-y-1">
                   <a
                     href="#"
-                    class="bg-gray-100 text-gray-900 group flex items-center px-2 py-2 text-base font-medium rounded-md"
+                    className="bg-gray-100 text-gray-900 group flex items-center px-2 py-2 text-base font-medium rounded-md"
                   >
                     <svg
-                      class="text-gray-500 mr-4 h-6 w-6"
+                      className="text-gray-500 mr-4 h-6 w-6"
                       xmlns="http://www.w3.org/2000/svg"
                       fill="none"
                       viewBox="0 0 24 24"
@@ -59,10 +59,10 @@ const dashboard = () => {
 
                   <a
                     href="#"
-                    class="text-gray-600 hover:bg-gray-50 hover:text-gray-900 group flex items-center px-2 py-2 text-base font-medium rounded-md"
+                    className="text-gray-600 hover:bg-gray-50 hover:text-gray-900 group flex items-center px-2 py-2 text-base font-medium rounded-md"
                   >
                     <svg
-                      class="text-gray-400 group-hover:text-gray-500 mr-4 h-6 w-6"
+                      className="text-gray-400 group-hover:text-gray-500 mr-4 h-6 w-6"
                       xmlns="http://www.w3.org/2000/svg"
                       fill="none"
                       viewBox="0 0 24 24"
@@ -81,10 +81,10 @@ const dashboard = () => {
 
                   <a
                     href="#"
-                    class="text-gray-600 hover:bg-gray-50 hover:text-gray-900 group flex items-center px-2 py-2 text-base font-medium rounded-md"
+                    className="text-gray-600 hover:bg-gray-50 hover:text-gray-900 group flex items-center px-2 py-2 text-base font-medium rounded-md"
                   >
                     <svg
-                      class="text-gray-400 group-hover:text-gray-500 mr-4 h-6 w-6"
+                      className="text-gray-400 group-hover:text-gray-500 mr-4 h-6 w-6"
                       xmlns="http://www.w3.org/2000/svg"
                       fill="none"
                       viewBox="0 0 24 24"
@@ -103,10 +103,10 @@ const dashboard = () => {
 
                   <a
                     href="#"
-                    class="text-gray-600 hover:bg-gray-50 hover:text-gray-900 group flex items-center px-2 py-2 text-base font-medium rounded-md"
+                    className="text-gray-600 hover:bg-gray-50 hover:text-gray-900 group flex items-center px-2 py-2 text-base font-medium rounded-md"
                   >
                     <svg
-                      class="text-gray-400 group-hover:text-gray-500 mr-4 h-6 w-6"
+                      className="text-gray-400 group-hover:text-gray-500 mr-4 h-6 w-6"
                       xmlns="http://www.w3.org/2000/svg"
                       fill="none"
                       viewBox="0 0 24 24"
@@ -125,10 +125,10 @@ const dashboard = () => {
 
                   <a
                     href="#"
-                    class="text-gray-600 hover:bg-gray-50 hover:text-gray-900 group flex items-center px-2 py-2 text-base font-medium rounded-md"
+                    className="text-gray-600 hover:bg-gray-50 hover:text-gray-900 group flex items-center px-2 py-2 text-base font-medium rounded-md"
                   >
                     <svg
-                      class="text-gray-400 group-hover:text-gray-500 mr-4 h-6 w-6"
+                      className="text-gray-400 group-hover:text-gray-500 mr-4 h-6 w-6"
                       xmlns="http://www.w3.org/2000/svg"
                       fill="none"
                       viewBox="0 0 24 24"
@@ -147,10 +147,10 @@ const dashboard = () => {
 
                   <a
                     href="#"
-                    class="text-gray-600 hover:bg-gray-50 hover:text-gray-900 group flex items-center px-2 py-2 text-base font-medium rounded-md"
+                    className="text-gray-600 hover:bg-gray-50 hover:text-gray-900 group flex items-center px-2 py-2 text-base font-medium rounded-md"
                   >
                     <svg
-                      class="text-gray-400 group-hover:text-gray-500 mr-4 h-6 w-6"
+                      className="text-gray-400 group-hover:text-gray-500 mr-4 h-6 w-6"
                       xmlns="http://www.w3.org/2000/svg"
                       fill="none"
                       viewBox="0 0 24 24"
@@ -168,14 +168,14 @@ const dashboard = () => {
                   </a>
                 </nav>
               </div>
-              <div class="flex-shrink-0 flex border-t border-gray-200 p-4">
-                <a href="#" class="flex-shrink-0 group block">
-                  <div class="flex items-center">
-                    <div class="ml-3">
-                      <p class="text-base font-medium text-gray-700 group-hover:text-gray-900">
+              <div className="flex-shrink-0 flex border-t border-gray-200 p-4">
+                <a href="#" className="flex-shrink-0 group block">
+                  <div className="flex items-center">
+                    <div className="ml-3">
+                      <p className="text-base font-medium text-gray-700 group-hover:text-gray-900">
                         Tom Cook
                       </p>
-                      <p class="text-sm font-medium text-gray-500 group-hover:text-gray-700">
+                      <p className="text-sm font-medium text-gray-500 group-hover:text-gray-700">
                         View profile
                       </p>
                     </div>
@@ -183,21 +183,21 @@ const dashboard = () => {
                 </a>
               </div>
             </div>
-            <div class="flex-shrink-0 w-14"></div>
+            <div className="flex-shrink-0 w-14"></div>
           </div>
         </div>
 
-        <div class="hidden md:flex md:flex-shrink-0">
-          <div class="flex flex-col w-64">
-            <div class="flex flex-col h-0 flex-1 border-r border-gray-200 ">
-              <div class="flex-1 flex flex-col pt-5 pb-4 overflow-y-auto">
-                <nav class="mt-5 flex-1 px-2 space-y-1">
+        <div className="hidden md:flex md:flex-shrink-0">
+          <div className="flex flex-col w-64">
+            <div className="flex flex-col h-0 flex-1 border-r border-gray-200 ">
+              <div className="flex-1 flex flex-col pt-5 pb-4 overflow-y-auto">
+                <nav className="mt-5 flex-1 px-2 space-y-1">
                   <a
                     href="#"
-                    class="bg-gray-100 text-gray-900 group flex items-center px-2 py-2 text-sm font-medium rounded-md"
+                    className="bg-gray-100 text-gray-900 group flex items-center px-2 py-2 text-sm font-medium rounded-md"
                   >
                     <svg
-                      class="text-gray-500 mr-3 h-6 w-6"
+                      className="text-gray-500 mr-3 h-6 w-6"
                       xmlns="http://www.w3.org/2000/svg"
                       fill="none"
                       viewBox="0 0 24 24"
@@ -216,10 +216,10 @@ const dashboard = () => {
 
                   <a
                     href="#"
-                    class="text-gray-600 hover:bg-gray-50 hover:text-gray-900 group flex items-center px-2 py-2 text-sm font-medium rounded-md"
+                    className="text-gray-600 hover:bg-gray-50 hover:text-gray-900 group flex items-center px-2 py-2 text-sm font-medium rounded-md"
                   >
                     <svg
-                      class="text-gray-400 group-hover:text-gray-500 mr-3 h-6 w-6"
+                      className="text-gray-400 group-hover:text-gray-500 mr-3 h-6 w-6"
                       xmlns="http://www.w3.org/2000/svg"
                       fill="none"
                       viewBox="0 0 24 24"
@@ -237,14 +237,14 @@ const dashboard = () => {
                   </a>
                 </nav>
               </div>
-              <div class="flex-shrink-0 flex border-t border-gray-200 p-4">
-                <a href="#" class="flex-shrink-0 w-full group block">
-                  <div class="flex items-center">
-                    <div class="ml-3">
-                      <p class="text-sm font-medium text-gray-700 group-hover:text-gray-900">
+              <div className="flex-shrink-0 flex border-t border-gray-200 p-4">
+                <a href="#" className="flex-shrink-0 w-full group block">
+                  <div className="flex items-center">
+                    <div className="ml-3">
+                      <p className="text-sm font-medium text-gray-700 group-hover:text-gray-900">
                         Tom Cook
                       </p>
-                      <p class="text-xs font-medium text-gray-500 group-hover:text-gray-700">
+                      <p className="text-xs font-medium text-gray-500 group-hover:text-gray-700">
                         View profile
                       </p>
                     </div>
@@ -254,12 +254,12 @@ const dashboard = () => {
             </div>
           </div>
         </div>
-        <div class="flex flex-col w-0 flex-1 overflow-hidden">
-          <div class="md:hidden pl-1 pt-1 sm:pl-3 sm:pt-3">
-            <button class="-ml-0.5 -mt-0.5 h-12 w-12 inline-flex items-center justify-center rounded-md text-gray-500 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-              <span class="sr-only">Open sidebar</span>
+        <div className="flex flex-col w-0 flex-1 overflow-hidden">
+          <div className="md:hidden pl-1 pt-1 sm:pl-3 sm:pt-3">
+            <button className="-ml-0.5 -mt-0.5 h-12 w-12 inline-flex items-center justify-center rounded-md text-gray-500 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+              <span className="sr-only">Open sidebar</span>
               <svg
-                class="h-6 w-6"
+                className="h-6 w-6"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
@@ -276,16 +276,18 @@ const dashboard = () => {
             </button>
           </div>
           <main
-            class="flex-1 relative z-0 overflow-y-auto focus:outline-none"
-            tabindex="0"
+            className="flex-1 relative z-0 overflow-y-auto focus:outline-none"
+            tabIndex="0"
           >
-            <div class="py-6">
-              <div class="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
-                <h1 class="text-2xl font-semibold text-gray-900">Dashboard</h1>
+            <div className="py-6">
+              <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
+                <h1 className="text-2xl font-semibold text-gray-900">
+                  Dashboard
+                </h1>
               </div>
-              <div class="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
-                <div class="py-4">
-                  <div class="border-4 border-dashed border-gray-200 rounded-lg h-96"></div>
+              <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
+                <div className="py-4">
+                  <div className="border-4 border-dashed border-gray-200 rounded-lg h-96"></div>
                 </div>
               </div>
             </div>
