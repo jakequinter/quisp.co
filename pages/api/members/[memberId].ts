21157bd1277f323d7d24c21a58c0001750645926
jakequinter <500132870@fvtc.edu>
2021-02-22 17:28:45 -0500

    removed unused API routes and pages

--- pages/api/members/[memberId].ts
+++
@@ -1,8 +0,0 @@
-import { getSingleMember } from "@/lib/db-admin"
-
-export default async (req, res) => {
-  const memberId = req.query.memberId;
-  const member = await getSingleMember(memberId);
-
-  res.status(200).json({ member })
-}
